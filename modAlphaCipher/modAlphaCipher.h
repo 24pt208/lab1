d2c9a99879882e8bb1238c63d6e6eba6b78bbc0d
24pt208 <neonkloyn@mail.ru>
2025-10-10 21:13:44 +0300

    zagruzka

---
+++ modAlphaCipher/modAlphaCipher.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+class modAlphaCipher
+{
+private:
+wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //алфавит по порядку
+    map<wchar_t, int> alphaNum; //ассоциативный массив "номер по символу"
+    vector<int> key; //ключ
+    vector<int> convert(const wstring& s); //преобразование строка-вектор
+    wstring convert(const vector<int>& v); //преобразование вектор-строка
+    
+public:
+    modAlphaCipher() = delete;
+    modAlphaCipher(const wstring& skey); //конструктор для установки ключа
+    wstring encrypt(const wstring& open_text); //зашифрование
+    wstring decrypt(const wstring& cipher_text);//расшифрование
+};
