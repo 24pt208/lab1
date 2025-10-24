2ca041b776f96e7d1e2e1953eb1bf14173c53cee
24pt208 <neonkloyn@mail.ru>
2025-10-24 22:32:29 +0300

    исправления

--- Module/module.h
+++ Module/module.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <string>
-#include <locale>
+#include <vector>
 
-class RouteCipher {
+class routeCipher
+{
 private:
-    int columns;
+    int key; // количество столбцов
     
+    // Вспомогательные методы
+    std::string getValidText(const std::string& text);
+    std::vector<std::vector<char>> createTable(const std::string& text, bool encrypt);
+    std::string readTableEncrypt(const std::vector<std::vector<char>>& table);
+    std::string readTableDecrypt(const std::vector<std::vector<char>>& table);
+
 public:
-    RouteCipher(int key);
-    void setKey(int key);
-    int getKey() const;
-    std::wstring encrypt(const std::wstring& text);
-    std::wstring decrypt(const std::wstring& text);
+    routeCipher() = delete;
+    routeCipher(int k);
+    
+    std::string encrypt(const std::string& text);
+    std::string decrypt(const std::string& text);
 };
